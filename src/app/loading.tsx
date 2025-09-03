c99d1b4f7ed2b6bf0366816ef79c6e2cdd3f158d
Yusup <yusuprd@icloud.com>
2025-09-03 21:17:37 +0500

    Updated loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,17 @@
 import { FaSpinner } from 'react-icons/fa6';
 
-export default function LoadingPage() {
+interface LoadingPageProps {
+  withRadialBg?: boolean;
+}
+
+export default function LoadingPage({ withRadialBg = false }: LoadingPageProps) {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div
+      className={`flex min-h-screen items-center justify-center ${withRadialBg ? 'radial-bg' : ''}`}
+    >
       <div className="flex flex-col items-center gap-4">
-        <FaSpinner className="size-5 animate-spin" />
-        <h2 className="font-semibold">Loading...</h2>
+        <FaSpinner className="size-8 animate-spin text-white" />
+        <h2 className="font-semibold text-white">Loading...</h2>
       </div>
     </div>
   );
