31c77488454c497112aea33f2d2d91e7077b45a2
Yusup <yusuprd@icloud.com>
2025-09-03 21:20:05 +0500

    Updated providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,20 @@
 import { AuthProvider } from '@/providers/AuthProvider';
+import { SocketProvider } from '@/providers/SocketProvider';
 import { ToastContainer } from 'react-toastify';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <AuthProvider>
-      {children}
-      <ToastContainer
-        autoClose={3000}
-        closeOnClick
-        pauseOnHover
-        draggable
-        style={{ fontFamily: 'inherit' }}
-      />
+      <SocketProvider>
+        {children}
+        <ToastContainer
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover
+          draggable
+          style={{ fontFamily: 'inherit' }}
+        />
+      </SocketProvider>
     </AuthProvider>
   );
 };
