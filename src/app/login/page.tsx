54216e9f8cd92d8211e29f10b8e29499bc19d150
Yusup <yusuprd@icloud.com>
2025-09-04 17:46:34 +0500

    Protected register and login routes to redirect if user already authenticated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,31 @@
+'use client';
+
+import { useLayoutEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/providers/AuthProvider';
 import LoginForm from '@/components/Forms/LoginForm';
 import Header from '@/components/Layout/Header';
 import Link from 'next/link';
+import LoadingPage from '@/app/loading';
 
 const Login = () => {
+  const router = useRouter();
+  const { isAuthenticated, loading } = useAuth();
+
+  useLayoutEffect(() => {
+    if (!loading && isAuthenticated) {
+      router.push('/');
+    }
+  }, [isAuthenticated, loading, router]);
+
+  if (loading) {
+    return <LoadingPage withRadialBg={true} />;
+  }
+
+  if (isAuthenticated) {
+    return null;
+  }
+
   return (
     <div className="radial-bg min-h-screen space-y-10">
       <Header />
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,31 @@
+'use client';
+
+import { useLayoutEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/providers/AuthProvider';
 import RegisterForm from '@/components/Forms/RegisterForm';
 import Link from 'next/link';
 import Header from '@/components/Layout/Header';
+import LoadingPage from '@/app/loading';
 
 const Register = () => {
+  const router = useRouter();
+  const { isAuthenticated, loading } = useAuth();
+
+  useLayoutEffect(() => {
+    if (!loading && isAuthenticated) {
+      router.push('/');
+    }
+  }, [isAuthenticated, loading, router]);
+
+  if (loading) {
+    return <LoadingPage withRadialBg={true} />;
+  }
+
+  if (isAuthenticated) {
+    return null;
+  }
+
   return (
     <div className="radial-bg min-h-screen space-y-10">
       <Header />
